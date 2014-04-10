6673dd5b462e7fdea675a240ceeb85401e1fc33b
rlamchops <lamraymond1996@gmail.com>
2014-04-09 21:00:55 -0400

    basic db? did not test yet

--- db.py
+++ db.py
@@ -1,1 +1,29 @@
 from pymongo import MongoClient
+
+def open():
+  client = MongoClient()
+  db = client.gm.users
+  return db
+  
+def register(username, password, nickname):
+    db = open()
+    check = db.find_one({'username' : username}, fields={'_id':False})
+    if check == None:
+        db.insert({'username' : username, 'password' : password, 'nickname' : nickname})
+        return True
+    else:
+        return False
+        
+def getNickName(username):
+    db = open()
+    check = db.find_one({'username':username}, fields={'_id':False})
+    return check['nickname']
+    
+def login(username, password):
+    db = open()
+    user = db.find_one({'username' : username, 'password' : password}, fields={'_id':False})
+    if user == None:
+        return False
+    else:
+        return True
+        
