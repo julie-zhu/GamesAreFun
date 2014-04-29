56028e916071c2d0a8f2ca226a21ca1e2473e639
rlamchops <lamraymond1996@gmail.com>
2014-04-28 20:08:14 -0400

    classes for the different chess pieces

--- utils.py
+++ utils.py
@@ -4,5 +4,42 @@ class User(object):
     self.nickname = nickname
     
 ________________________________________
-#unsure if to make separate classes or to just make one
-
+#unsure if to make separate classes for pieces or to just make one
+#______________________CHESSMEN_______________________________
+class Pawn(object):
+  def __init__(self, x, y, color, promo): #promo(tion) is there in case it gets promoted
+    self.x = x
+    self.y = y #location
+    self.color = color #team side
+    self.promo = promo
+    
+class Rook(object):
+  def __init__(self, x, y, color):
+    self.x = x
+    self.y = y
+    self.color = color
+    
+class Knight(object):
+  def __init__(self, x, y, color):
+    self.x = x
+    self.y = y
+    self.color = color
+    
+class Bishop(object):
+  def __init__(self, x, y, color):
+    self.x = x
+    self.y = y
+    self.color = color
+    
+class Queen(object):
+  def __init__(self, x, y, color):
+    self.x = x
+    self.y = y
+    self.color = color
+    
+class King(object):
+  def __init__(self, x, y, color):
+    self.x = x
+    self.y = y
+    self.color = color
+    
