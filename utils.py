9a8de3f416bef79c4075196275c177608a39a049
rlamchops <lamraymond1996@gmail.com>
2014-04-28 20:02:34 -0400

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+class User(object):
+  def __init__(self, username, nickname):
+    self.username = username
+    self.nickname = nickname
+    
+________________________________________
+#unsure if to make separate classes or to just make one
+
