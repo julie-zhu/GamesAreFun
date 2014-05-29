2aa0091358ef809d98bf05e52d0cdf289d048f0b
rlamchops <lamraymond1996@gmail.com>
2014-05-29 12:11:35 -0400

    descriptions to methods, some more fixing

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ from utils import User, Pawn, Rook, Knight, Bishop, Queen, King, Room
 app = Flask(__name__)
 app.secret_key = "asdfghjkl"
 
+#player ID's
 black = 1
 white = 2
 spectator = 3
+
+#move pools. assumes board is from 1-64 (8x8 square)
 knightMoves = [6, 10, 15, 17]
 pawnMoves = [7, 8, 9]
-bishopMoves = [9,11]
-kingMoves = [0, 1, 2, 3, 4, 5, 6, 7]
+kingMoves = [0, 1, 7, 9]
 
+#checkMove returns true if a piece's movement is legal, false otherwise
 def checkMove(piece, oPosition, nPosition):
 	if piece == "Knight":
 		if abs(oPosition - nPosition) in knightMoves:
@@ -59,6 +62,7 @@ def checkMove(piece, oPosition, nPosition):
 		else:
 			return False
 
+#dunno , just kept this here in case it's needed
 def auth(func):
 	def wrapper(*args):
 		if "username" in session:
@@ -67,10 +71,12 @@ def auth(func):
 			return redirect("/login")
 	return wrapper
 
+#homepage, needs to have navigation to board page
 @app.route("/")
 def home():
 	return render_template("home.html")
 
+#not needed, will delete later
 @app.route("/login", methods=["GET", "POST"])
 def login():
 	if request.method == "GET":
@@ -86,6 +92,9 @@ def login():
 		else :
 			return redirect("/secret2")
 
+#pieceMoved should, given the piece, its original position, and its new position, check to see
+#if the move is valid. if valid, should return true and save the new position of the piece into the db.
+#if not valid, return false and do nothing at all.
 @app.route("/pieceMoved")
 def pieceMoved():
 	a = checkMove()
@@ -94,16 +103,19 @@ def pieceMoved():
 	else:
 		#put piece back in original position and return false
 
+#showBoard should get x and y coor's of all active pieces (meaning not taken/defeated) and show them
 @app.route("/showBoard")
 def showBoard():
 	#placeholder
 
+#here for reference
 @app.route("/dosomething")
 def something():
 	a = request.args.get('a', 0, type=int)
 	b = request.args.get('b', 0, type=int)
 	return jsonify(result = a + b)
 
+#logout should remove the ID of the user (1, 2, or 3) so that other people can play/spectate
 @app.route("/logout")
 def logout():
 	session.pop("username", None)
