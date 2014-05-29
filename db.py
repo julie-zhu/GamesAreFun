cf7af07f97f2bf6afe0589c09cf9e78aecbba6f3
rlamchops <lamraymond1996@gmail.com>
2014-05-29 12:29:19 -0400

    got rid of unnecessary stuff

--- db.py
+++ db.py
@@ -2,28 +2,9 @@ from pymongo import MongoClient
 
 def open():
   client = MongoClient()
-  db = client.gm.users
+  db = client.pieces
   return db
   
-def register(username, password, nickname):
-    db = open()
-    check = db.find_one({'username' : username}, fields={'_id':False})
-    if check == None:
-        db.insert({'username' : username, 'password' : password, 'nickname' : nickname})
-        return True
-    else:
-        return False
-        
-def getNickName(username):
-    db = open()
-    check = db.find_one({'username':username}, fields={'_id':False})
-    return check['nickname']
-    
-def login(username, password):
-    db = open()
-    user = db.find_one({'username' : username, 'password' : password}, fields={'_id':False})
-    if user == None:
-        return False
-    else:
-        return True
-        
+def newBoard(): #or newGame? hmmm
+  #should create 2 arrays, 1 for black, 1 for white
+  #these should keep track of position and status (taken or not)
