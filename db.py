1d5429ff8e479352540734e01b67b2ed0ca01e90
rlamchops <lamraymond1996@gmail.com>
2014-06-03 12:04:07 -0400

    should work for 2 boards now

--- db.py
+++ db.py
@@ -11,9 +11,71 @@ def addPiece(piece, color, x, y):
   db.insert({'piece': piece, 'color': color, 'x': x, 'y': y})
   return True
 
-#one array of 32 pieces
+#one array of 32 pieces (2X for bughouse)
 def newBoard(): #or newGame? hmmm
   db = open()
+  db.remove({}) #to remove any pieces that may already exist
+  
+  for x in range(0, 8):
+    addPiece("Pawn", "black1", 7, x)
+    
+  for x in range(0, 8):
+    addPiece("Pawn", "white1", 1, x)
+    
+  # rows and columns are numbered from 0 to 7  (0,0) is the bottom left corner
+  addPiece("Rook", "black1", 7, 0)
+  addPiece("Rook", "black1", 7, 7)
+  addPiece("Rook", "white1", 0, 0)
+  addPiece("Rook", "white1", 0, 7)
+  
+  addPiece("Knight", "black1", 7, 1)
+  addPiece("Knight", "black1", 7, 6)
+  addPiece("Knight", "white1", 0, 1)
+  addPiece("Knight", "white1", 0, 6)
+  
+  addPiece("Bishop", "black1", 7, 2)
+  addPiece("Bishop", "black1", 7, 5)
+  addPiece("Bishop", "white1", 0, 2)
+  addPiece("Bishop", "white1", 0, 5)
+  
+  addPiece("King", "black1", 7, 4)
+  addPiece("King", "white1", 0, 4)
+  
+  addPiece("Queen", "black1", 7, 3)
+  addPiece("Queen", "white1", 0, 3)
+  return True
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+  for x in range(0, 8):
+    addPiece("Pawn", "black2", 7, x)
+    
+  for x in range(0, 8):
+    addPiece("Pawn", "white2", 1, x)
+    
+  addPiece("Rook", "black2", 7, 0)
+  addPiece("Rook", "black2", 7, 7)
+  addPiece("Rook", "white2", 0, 0)
+  addPiece("Rook", "white2", 0, 7)
+  
+  addPiece("Knight", "black2", 7, 1)
+  addPiece("Knight", "black2", 7, 6)
+  addPiece("Knight", "white2", 0, 1)
+  addPiece("Knight", "white2", 0, 6)
+  
+  addPiece("Bishop", "black2", 7, 2)
+  addPiece("Bishop", "black2", 7, 5)
+  addPiece("Bishop", "white2", 0, 2)
+  addPiece("Bishop", "white2", 0, 5)
+  
+  addPiece("King", "black2", 7, 4)
+  addPiece("King", "white2", 0, 4)
+  
+  addPiece("Queen", "black2", 7, 3)
+  addPiece("Queen", "white2", 0, 3)
+  return True
+#-------------------------------------------------------------------------------------
+def newBoard2(): #or newGame? hmmm (1X board's worth of pieces)
+  db = open()
+  db.remove({}) #to remove any pieces that may already exist
   
   for x in range(0, 8):
     addPiece("Pawn", "black", 7, x)
@@ -43,10 +105,11 @@ def newBoard(): #or newGame? hmmm
   addPiece("Queen", "black", 7, 3)
   addPiece("Queen", "white", 0, 3)
   return True
-  
+
+
 #finds a piece and updates its coordinates and/or status
 def updatePiece(piece, color, x, y):
   db = open()
   piece = db.find_one({'piece': piece, 'color':color}, fields={'_id': False})
   db.update({'piece': piece}, {'$set':{'x': x, 'y': y}})
-  
+  return True
